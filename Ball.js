74f2f09512515f81e50354b1dd33c82cf4ce6419
AiShuvo <aislam12mr92@gmail.com>
2017-03-07 09:08:58 -0800

    modified

--- Ball.js
+++ Ball.js
@@ -5,7 +5,13 @@ function Ball(){
 	var Y = 0;
 	var width = 10;
 	var height = 10;
-	
+	var xPos = 0;//left sidei distane upto ball can move.........
+	var yPos = 0;//Up side distance upto which ball can move............
+	var left = false;
+	var right = true;
+	var up = true;
+	var down = false;
+	var canvas;
 	this.connection = function(){
 		
 		alert("Ball is Connected");
@@ -15,15 +21,73 @@ function Ball(){
 		
 		this.canvasId = canvasId;
 	}
-	
+	this.setPosition = function(x,y){
+		X = x;
+		Y = y;
+	}
 	this.draw = function(){
-		
 		var ctx = this.canvasId.getContext("2d");
-		
 		ctx.fillStyle = color;
 		ctx.fillRect(X,Y,width,height);
 	}
+	this.run = function(canv){
+		this.canvas = canv;
+		setInterval(this.changePosition,10);
+	}
+	
 	this.changePosition = function(){
-		
+			this.canvas.draw();
+			if(right && up ){
+			X++;
+			Y--;
+			if(X>=this.canvasId.width){
+				right = false;
+				left = true;
+			}
+			if(Y<=yPos){
+				up = false;
+				down = true;
+			}
+		}else if(left && up){
+			X--;
+			Y--;
+			if(X<=xPos){
+				left = false;
+				right = true;
+			}
+			if(Y<=yPos){
+				up = false;
+				down = true;
+			}
+		//		this.setDirection(true,false,false,true);
+		}else if(left && down){
+			X--;
+			Y++;
+			
+			if(X<=xPos){
+				left = false;
+				right = true;
+			}
+			if(Y>=this.canvasId.height){
+				down = false;
+				up = true;
+			}
+			//	this.setDirection(false,true,false,true)
+		}else if(right && down){
+			X++;
+			Y++
+			if(X>=this.canvasId.width){
+				right = false;
+				left = true;
+			}
+			if(Y>=this.canvasId.height){
+				down = false;
+				up = true;
+			}
+//				this.setDirection(false,true,true,false);
+		}
+	}
+	this.setMovingSide = function(){
+		alert("hello");
 	}
 }
