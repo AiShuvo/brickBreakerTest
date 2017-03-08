af64246647a891ca52e4d83534448b8bf93930f5
AiShuvo <aislam12mr92@gmail.com>
2017-03-08 05:50:32 -0800

    modified

--- Ball.js
+++ Ball.js
@@ -1,17 +1,20 @@
 function Ball(){
 	var canvasId;
-	var color = "red";
+	var color = "white";
+	var color2  = "red"
 	var X = 0;
 	var Y = 0;
 	var dimension = 10;
 	var dimensionInPercentage = 3;
 	var xPos = 0;//left sidei distane upto ball can move.........
 	var yPos = 0;//Up side distance upto which ball can move............
-	var left = false;
-	var right = true;
+	var left = true;
+	var right = false;
 	var up = true;
 	var down = false;
 	var canvas;
+	var step = 1;
+	var sSurface;
 	this.connection = function(){
 		
 		alert("Ball is Connected");
@@ -31,15 +34,18 @@ function Ball(){
 		var ctx = this.canvasId.getContext("2d");
 		ctx.fillStyle = color;
 		ctx.fillRect(X,Y,dimension,dimension);
+		ctx.fillStyle = color2;
+		ctx.fillRect(X+2,Y+2,dimension-4,dimension-4);
 	}
 	this.run = function(){
+
 		setInterval(this.changePosition,0);
 	}
 	
 	this.changePosition = function(){
 			if(right && up ){
-			X++;
-			Y--;
+			X+=step;
+			Y-=step;
 			if(X+dimension>=this.canvasId.width){
 				right = false;
 				left = true;
@@ -49,8 +55,8 @@ function Ball(){
 				down = true;
 			}
 		}else if(left && up){
-			X--;
-			Y--;
+			X-=step;
+			Y-=step;
 			if(X<=xPos){
 				left = false;
 				right = true;
@@ -59,35 +65,47 @@ function Ball(){
 				up = false;
 				down = true;
 			}
-		//		this.setDirection(true,false,false,true);
 		}else if(left && down){
-			X--;
-			Y++;
+			X-=step;
+			Y+=step;
 			
 			if(X<=xPos){
 				left = false;
 				right = true;
 			}
-			if(Y+dimension>=this.canvasId.height){
+			if(Y+dimension==sSurface.y && X<=sSurface.x+sSurface.w && X+dimension>=sSurface.x){
 				down = false;
 				up = true;
 			}
-			//	this.setDirection(false,true,false,true)
+			if(X <= sSurface.x+sSurface.w && X+dimension>=sSurface.x+sSurface.w && Y+dimension>=sSurface.y && Y<=sSurface.y+sSurface.w){
+				right = true;
+				left = false;
+				down = true;
+				up = false;
+			}
 		}else if(right && down){
-			X++;
-			Y++
-			if(X>=this.canvasId.width){
+			X+=step;
+			Y+=step
+			if(X+dimension>=this.canvasId.width){
 				right = false;
 				left = true;
 			}
-			if(Y>=this.canvasId.height){
+			if(Y+dimension==sSurface.y && X<=(sSurface.x+sSurface.w) && X+dimension>=sSurface.x){
 				down = false;
 				up = true;
 			}
-//				this.setDirection(false,true,true,false);
+			if(X <= sSurface.x && X+dimension>=sSurface.x && Y+dimension>=sSurface.y && Y<=sSurface.y+sSurface.w){
+				right = false;
+				left = true;
+				down = true;
+				up = false;
+			}
 		}
 	}
-	this.setMovingSide = function(){
-		alert("hello");
+	this.setSpring = function(sps){
+		sSurface = sps;		
+	}
+	this.getDimension = function(){
+		return dimension;
 	}
 }
--- SpringSurface.js
+++ SpringSurface.js
@@ -8,9 +8,17 @@ function SpringSurface(){
 	var widthInPercentage = 40;
 	var heighInPercentage = 3;
 	var yInPercentage = 90;
-	var step = 10;
+	var step = 20;
 	var color = "white";
 	var color2 = "red";
+	
+	var springObject = {
+						x:X,
+						y:Y,
+						w:width,
+						h:height
+						};
+	
 	this.connection = function(){
 		
 		alert("SpringSurface is connected");
@@ -22,6 +30,7 @@ function SpringSurface(){
 		width = canvasId.width * widthInPercentage/100;
 		height = canvasId.height * heighInPercentage/100;
 		Y = canvasId.height * yInPercentage/100;
+		this.updateSpringObject();
 	}
 	
 	this.draw = function(){		
@@ -32,6 +41,7 @@ function SpringSurface(){
 		ctx.fillStyle = color2;
 		ctx.fillRect (X+2,Y+2,width-4,height-4);
 	}
+	
 	this.changePosition = function(value){
 		
 		if(value == "right"){
@@ -44,8 +54,32 @@ function SpringSurface(){
 			if(X<posX)
 				X+=step;			
 		}
+		this.updateSpringObject();//update spring surface property every time when it move;
 	}
+	
 	this.setX = function(value){
 		X = value-width/2;
+	}
+	
+	this.getX = function(){
+		return X;
+	}
+	this.getY = function(){
+		return Y;
+	}
+	this.getWidht = function(){
+		return width;
+	}
+	this.getHeight = function(){
+		return height;
+	}
+	this.updateSpringObject = function(){
+		springObject.x = this.getX();
+		springObject.y = this.getY();
+		springObject.w = this.getWidht();
+		springObject.h = this.getHeight();
+	}
+	this.springProperty = function(){
+		return springObject;
 	}
 }
